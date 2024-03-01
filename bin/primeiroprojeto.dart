71e9becc015b51b2de27a5dce4dba20626a87b95
raibolato <rai.carvalho98@gmail.com>
2024-03-01 00:20:10 -0300

    primeiro commit

---
+++ bin/primeiroprojeto.dart
@@ -0,0 +1,5 @@
+import 'package:primeiroprojeto/primeiroprojeto.dart' as primeiroprojeto;
+
+void main(List<String> arguments) {
+  print('Hello world: ${primeiroprojeto.calculate()}!');
+}
---
+++ test/primeiroprojeto_test.dart
@@ -0,0 +1,8 @@
+import 'package:primeiroprojeto/primeiroprojeto.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
